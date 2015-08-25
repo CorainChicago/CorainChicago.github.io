f2bb25d94edea5d51d869f6d7ebb851299c10c77
CorainChicago <chaysmagan@gmail.com>
2015-08-24 19:01:32 -0500

    update blog on diversity

--- blog/test.rb
+++ blog/test.rb
@@ -1,36 +1,7 @@
-def method_each(list)
-	list.each {|x| x * 3}
-end
 
-def method_map(list)
-	new_list = list.map {|x| x * 3}
-end
+nested_array = [1, ["inner", "array"], 2, 3]
+nested_hash = {:outer_key => {:inner_key => "Winner!"}}
+desserts = {:favorite_dessert => "yellow cake", "second favorite dessert" => "cookies"}
 
-def method_map!(list)
-	list.map! {|x| x * 3}
-end
-
-list = ([1, 2, 3])
-new_list = []
-method_each(list)
-puts "list each"
-puts list
-puts "list new_list each"
-puts new_list
-
-puts "Each method"
-
-list = ([1, 2, 3])
-method_map(list)
-puts "list map"
-puts list
-puts "new_list map"
-puts new_list
-
-ist = ([1, 2, 3])
-method_map!(list)
-puts "list map!"
-puts list
-puts "new_list map!"
-puts new_list
-
+nested_array.each {|element| p element}
+p "puts nested_array" + nested_array
