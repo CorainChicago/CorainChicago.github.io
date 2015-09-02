36f9854f44b32292d0366abb55bf7c2493d3febc
CorainChicago <chaysmagan@gmail.com>
2015-09-01 20:26:29 -0500

    Update class blog

--- blog/test.rb
+++ blog/test.rb
@@ -1,7 +1,63 @@
+class  Car2
+	def initialize (color, make, model, licence_plate)
+		@color = color
+		@make = make
+		@model = model
+		@licence_plate = licence_plate
+	end 
 
-nested_array = [1, ["inner", "array"], 2, 3]
-nested_hash = {:outer_key => {:inner_key => "Winner!"}}
-desserts = {:favorite_dessert => "yellow cake", "second favorite dessert" => "cookies"}
+	def describe_car
+		puts "It\s a #{@color} with a #{@make} engine, #{@model} wheels, and the licence plate number is #{@licence_plate}."
 
-nested_array.each {|element| p element}
-p "puts nested_array" + nested_array
+	end
+
+end
+test_car_2 = Car2.new('red', 'Toyota', 'Camry', 'WJE 910')
+puts test_car_2.describe_car
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class  Car1
+	def initialize(body, engine, wheels, steering)
+		@body = body
+		@engine = engine
+		@wheels= wheels
+		@steering = steering
+	end
+
+	def describe_car
+		puts "It\s a #{@body} with a #{@engine} engine, #{@wheels} wheels, and #{@steer} steering."
+	end
+	
+	
+end
+
+test_car_1 = Car1.new('sedan', 'V8', '235/30R20', 'power')
+
+puts test_car_1.describe_car
