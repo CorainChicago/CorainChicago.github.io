1d779e391d48f4675dffe392f711ca4846079b0e
CorainChicago <chaysmagan@gmail.com>
2015-09-07 21:50:26 -0500

    update photos

--- blog/test.rb
+++ blog/test.rb
@@ -1,24 +1,22 @@
 
 
-numbers = [3, 4, 5]
+
 
 #Bloc
+puts "This is with bloc"
+p [3,4,5].each {|num| num + 1}
 
-p numbers.each {|num| num + 1}
-
-p numbers.each do |num|
-	num + 1
-end
 
 #Proc
+puts "This is with proc"
 add_one = Proc.new {|num| num + 1}
 
-p add_one.map!(numbers)
+p add_one.(3,4,5)
 
 #Lambda
 puts "This is with lambda"
 add_one_lambda = lambda {|num| num + 1}
-p numbers.add_one_lambda
+p add_one_lambda.call(3)
 
 
 
@@ -29,40 +27,6 @@ p numbers.add_one_lambda
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class  Car1
-	def initialize(body, engine, wheels, steering)
-		@body = body
-		@engine = engine
-		@wheels= wheels
-		@steering = steering
-	end
-
-	def describe_car
-		puts "It's a #{@body} with a #{@engine} engine, #{@wheels} wheels, and #{@steer} steering."
-	end
-	
-	
-end
-
-test_car_1 = Car1.new('sedan', 'V8', '235/30R20', 'power')
-
-puts test_car_1.describe_car
 
 
 
